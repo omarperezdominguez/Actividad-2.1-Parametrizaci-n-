4e0ffa72fb3b00384d58a889df232429446c7ee9
omarperezdominguez <omarperezdominguez02@gmail.com>
2026-04-06 23:41:54 -0600

    Update Actividad_1_codigo.m

--- Actividad_1_codigo.m
+++ Actividad_1_codigo.m
@@ -4,7 +4,7 @@
 
 clear all; close all; clc;
 
-%% TRAYECTORIA 1: El Círculo (Radio = 2)
+% TRAYECTORIA 1: El Círculo (Radio = 2)
 % Se define t de 0 a 2*pi para cerrar el ciclo completo.
 t1 = 0:0.01:2*pi;
 x1 = 2 * cos(t1);
@@ -16,7 +16,7 @@ grid on; axis equal;
 title('1. Trayectoria Circular (R=2)');
 xlabel('x [m]'); ylabel('y [m]');
 
-%% TRAYECTORIA 2: Figura en Infinito (Lissajous)
+% TRAYECTORIA 2: Figura en Infinito (Lissajous)
 % La clave aquí es que la frecuencia en Y es el doble que en X.
 t2 = 0:0.01:2*pi;
 x2 = sin(t2); 
@@ -28,7 +28,7 @@ grid on; axis equal;
 title('2. Curva de Lissajous (Infinito)');
 xlabel('x [m]'); ylabel('y [m]');
 
-%% TRAYECTORIA 3: Parametrización Compleja (Epitrocoide)
+% TRAYECTORIA 3: Parametrización Compleja (Epitrocoide)
 % Se combinan dos movimientos circulares: uno lento y uno rápido (15 veces más)
 t3 = 0:0.005:2*pi;  
 x3 = 1.5 * cos(t3) + cos(15 * t3);
